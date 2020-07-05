d74d8ee71ab2d54993ea93ce9972d3a3f74a0cb6
EpicGibbon <michant1997@gmail.com>
2020-07-05 12:58:02 -0400

    The initial Fswritefile is working

--- index.js
+++ index.js
@@ -3,64 +3,65 @@ const fs = require("fs");
 const util = require("util");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-
-inquirer
-    .prompt([
-        {
-            type: "input",
-            message: "What would you like the Title to be?",
-            name: "titleval"
-        },
-        {
-            type: "input",
-            message: "What would you like the description content to be?",
-            name: "description"
-        },
-        {
-            type: "input",
-            message: "What will your table of contents be?",
-            name: "toc"
-        },
-        {
-            type: "input",
-            message: "What are your installation instructions?",
-            name: "install"
-        },
-        {
-            type: "input",
-            message: "What is your user usage?",
-            name: "description"
-        },
-        {
-            type: "input",
-            message: "What is are the contribution guidelines?",
-            name: "contrguidelines"
-        },
-        {
-            type: "input",
-            message: "What are the test instructions?",
-            name: "testinstr"
-        }
-    ])
-    .then(function (response) {
-
-        if (response.confirm === response.password) {
-            console.log("");
-        }
-        else {
-            console.log("");
-        }
-    });
-
-    console.log();
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        message: "What would you like the Title to be?",
+        name: "titleval"
+    },
+    {
+        type: "input",
+        message: "What would you like the description content to be?",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "What will your table of contents be?",
+        name: "toc"
+    },
+    {
+        type: "input",
+        message: "What are your installation instructions?",
+        name: "install"
+    },
+    {
+        type: "input",
+        message: "What is your user usage?",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "What is are the contribution guidelines?",
+        name: "contrguidelines"
+    },
+    {
+        type: "input",
+        message: "What are the test instructions?",
+        name: "testinstr"
+    }
 ];
 
+
+//This calls the questions
+inquirer.prompt(questions)
 // function to write README file
-function writeToFile(fileName, data) {
-}
+.then(function (data) {
+        //Taking the title name and giving the document that name
+        var readMe = data.name + ".md";
+        fs.writeFile(readMe, generateMarkdown(data), function (err) {
+            //handle an error if there is one
+            if (err) {
+                return console.log(err);
+            }
+            //else return "Sucess!"
+            console.log("success");
+        });
+});
+
+
+
+
 
 // function to initialize program
 function init() {
